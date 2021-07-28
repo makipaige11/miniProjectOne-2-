2708f7d5122685642db2d39f1aaf795a45fd82fb
Paige <makipaige11@gmail.com>
2021-07-28 18:41:31 -0500

    Initial Commit

---
+++ miniProjectOne(2)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  miniProjectOne(2)
+//
+//  Created by Paige Maki on 7/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
