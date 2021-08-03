33a1c7a5f5833fd355afc4aa37489a77255de97b
Paige <makipaige11@gmail.com>
2021-08-03 13:41:22 -0500

    Initial commit

--- miniProjectOne(2)/ViewController.swift
+++ miniProjectOne(2)/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appTitle: UILabel!
+    @IBOutlet weak var myName: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+//hopefully this shows up in github
+    @IBAction func answerOne(_ sender: UIButton) {
+        appTitle.text = "Truth :)"
+    }
+    
+    @IBAction func answerTwo(_ sender: UIButton) {
+        appTitle.text = "Lie, her name is Finnick"
+    }
+    @IBAction func answerThree(_ sender: UIButton) {
+        appTitle.text = "Truth :)"
+    }
 }
 
